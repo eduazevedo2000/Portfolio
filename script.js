dafe5d51d62129df356f253a2655392d667903a7
edu <eazevedo243@gmail.com>
2024-11-13 01:46:56 +0000

    unpacking

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 const projectDetails = document.getElementById('projectDetails')
 
-function projectDetailsButton() {
+// Function to update project details based on the active image
+function updateProjectDetails() {
   const activeImage = document.querySelector('.carousel-item.active img')
   if (activeImage.src.endsWith('listify.png')) {
-    projectDetails.href = 'https://github.com/eduazevedo2000/CalculatorIOS'
+    projectDetails.href = 'https://github.com/eduazevedo2000/Listify'
     console.log('hello')
   } else if (activeImage.src.endsWith('calculatorIOS.png')) {
-    projectDetails.href = 'https://github.com/eduazevedo2000/Listify'
+    projectDetails.href = 'https://github.com/eduazevedo2000/CalculatorIOS'
     console.log('world')
   } else if (activeImage.src.endsWith('portfolio.jpeg')) {
     projectDetails.href = 'https://github.com/eduazevedo2000/Portfolio'
-    console.log('world')
+    console.log('portfolio')
   } else if (activeImage.src.endsWith('vilaDoConde.jpeg')) {
     projectDetails.href = 'https://tiwm23tig06.netlify.app/'
-    console.log('world')
+    console.log('vilaDoConde')
   }
 }
 
-document
-  .getElementById('prevButton')
-  .addEventListener('click', projectDetailsButton)
-document
-  .getElementById('nextButton')
-  .addEventListener('click', projectDetailsButton)
+// Add event listener to the carousel for the 'slid.bs.carousel' event
+const carousel = document.getElementById('carouselExampleControls')
+carousel.addEventListener('slid.bs.carousel', updateProjectDetails)
